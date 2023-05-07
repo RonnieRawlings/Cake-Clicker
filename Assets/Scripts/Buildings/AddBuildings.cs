4f1547ad4c26d46e8bd9a0de73a08305ae08e405
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-05-07 13:34:48 +0100

    Implemented - Max Clicker Building Logic
    This commit implements logic for the Clicker Building to prevent the player purchasing more than the max amount of Clicker Buildings. Once max buildings is achieved the button is greyed out and the owned amount is changed to MAX.
    
    Next, start work on the visual overhall starting with giving the borders more texture.

--- Assets/Scripts/Buildings/AddBuildings.cs
+++ Assets/Scripts/Buildings/AddBuildings.cs
@@ -16,33 +16,61 @@ public class AddBuildings : MonoBehaviour
         TextMeshProUGUI amountOwnedText = transform.GetChild(0).GetChild(3).GetComponent<TextMeshProUGUI>();       
 
         int upgradePrice = int.Parse(priceText.text);
+        bool allEnabled = true;
 
         if (StaticValues.currentCakes >= upgradePrice) 
         {
-            StaticValues.cakesPerSecond += StaticValues.clickerCPS;
-            StaticValues.totalClickerCPS += StaticValues.clickerCPS;
-            StaticValues.currentCakes -= upgradePrice;
-            IncreaseUpgradePrice(priceText, upgradePrice);
-
             foreach (GameObject clicker in StaticValues.clickers)
             {
                 if (!clicker.GetComponent<Image>().enabled)
                 {
                     clicker.GetComponent<Image>().enabled = true;
+                    allEnabled = true;
+
+                    foreach (GameObject otherClicker in StaticValues.clickers)
+                    {
+                        if (!otherClicker.GetComponent<Image>().enabled)
+                        {
+                            allEnabled = false;
+                            break;
+                        }
+                    }
+
                     break;
                 }
             }
 
-            if (amountOwnedText.text == "")
+            if (allEnabled)
             {
-                amountOwnedText.text = "1";
+                PreventFurtherClickers(amountOwnedText, priceText);
             }
             else
             {
-                int currentOwned = int.Parse(amountOwnedText.text);
-                amountOwnedText.text = (currentOwned + 1).ToString();
-            }
+                if (amountOwnedText.text == "")
+                {
+                    amountOwnedText.text = "1";
+                }
+                else
+                {
+                    int currentOwned = int.Parse(amountOwnedText.text);
+                    amountOwnedText.text = (currentOwned + 1).ToString();
+                }
+
+                StaticValues.cakesPerSecond += StaticValues.clickerCPS;
+                StaticValues.totalClickerCPS += StaticValues.clickerCPS;
+                StaticValues.currentCakes -= upgradePrice;
+                IncreaseUpgradePrice(priceText, upgradePrice);
+            }           
         }      
+    }
+
+    public void PreventFurtherClickers(TextMeshProUGUI amountOwnedText, TextMeshProUGUI priceText)
+    {
+        amountOwnedText.text = "MAX";
+        priceText.text = "-";
+
+        GameObject clickerBuilding = transform.GetChild(0).gameObject;
+        clickerBuilding.GetComponent<Button>().interactable = false;
     }
 
     public void IncreaseUpgradePrice(TextMeshProUGUI textToChange, int currentPrice)
