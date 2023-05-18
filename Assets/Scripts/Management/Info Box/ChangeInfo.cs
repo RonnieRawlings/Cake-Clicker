41de6d09c1b55de15c4fe9b4f0af4f2031653011
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-05-18 16:58:49 +0100

    Implemented - CakeSprite Text Asset
    This commit adds a CakeSprite Text Asset to the info box prices, this is similar to what Cookie Clicker does and therefore looks more professional.
    
    Next, start work on implementing a 4th building.

--- Assets/Scripts/Management/Info Box/ChangeInfo.cs
+++ Assets/Scripts/Management/Info Box/ChangeInfo.cs
@@ -17,7 +17,7 @@ public class ChangeInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     {
         titleText.text = title;
         infoText.text = info;     
-        priceText.text = int.Parse(price).ToString("N0");
+        priceText.text = "<sprite=0> " + int.Parse(price).ToString("N0");
         cpsText.text = FindCPSValue();
         infoBackground.enabled = true;
     }
