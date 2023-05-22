42bae6c80a97e0b9cc21d7fb6cc6b047c3a3f229
Ronnie Rawlings <>
2023-05-22 13:12:00 +0100

    Implemented - SystemUnlock + Added - Pet Egg 1
    This commit implements the SystemUnlock Script which can unlock the Pet System when the player reaches the correct amount of progress. The first PetEgg Sprite has also been added with all it's crack variations.
    
    Next, start work on adding the first purchaseable egg to the Pet Store.

---
+++ Assets/Scripts/Pet System/SystemUnlock.cs
@@ -0,0 +1,25 @@
+// Author - Ronnie Rawlings.
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SystemUnlock : MonoBehaviour
+{
+    private int unlockAmount = 3000;
+    
+    public void UnlockSystemMeth()
+    {
+        if (StaticValues.currentCakes >= unlockAmount)
+        {
+            transform.Find("StoreTitle").GetComponent<TextMeshProUGUI>().text = "Pet Store";
+            Destroy(this);
+        }
+    }
+
+    void Update()
+    {
+        UnlockSystemMeth();
+    }
+}
