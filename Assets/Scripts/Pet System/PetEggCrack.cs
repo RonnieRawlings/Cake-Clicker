0f20119efecbf033a7020b41fc2c3dc3aedb9d09
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-05-24 14:33:22 +0100

    Implemented - Egg Cracking Script
    This commit removes the EggHoverShake Script and replaces it with the PetEggCrack Script which allows the player to click on the PetEgg putting more cracks in it each time. This will eventually lead to the player obtaining a Pet which gives them Cake bonuses.
    
    Next, design and add a pet sprite.

--- Assets/Scripts/Pet System/PetEggCrack.cs
+++ Assets/Scripts/Pet System/PetEggCrack.cs
@@ -1,17 +1,18 @@
 // Author - Ronnie Rawlings.
 
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class EggHoverAnim : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class PetEggCrack : MonoBehaviour
 {
-    public float rotationAmount = 15f, rotationSpeed = 1f, rotationPause = 0.5f;
+    [SerializeField] private List<Sprite> petEggSprites = new List<Sprite>();
+    [SerializeField] private float rotationAmount = 15f, rotationSpeed = 1f, rotationPause = 0.5f;
 
+    private int currentSprite = 0;
     private Image imageToRotate;
     private Quaternion originalRotation;
-    private Coroutine rotateCoroutine;
 
     void Start()
     {
@@ -19,36 +20,27 @@ public class EggHoverAnim : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
         originalRotation = imageToRotate.transform.localRotation;
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    public void StartCrackEgg()
     {
-        rotateCoroutine = StartCoroutine(RotateCoroutine());
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        StopRotate();
-    }
-
-    public void StopRotate()
-    {
-        if (rotateCoroutine != null)
+        if (currentSprite != petEggSprites.Count)
         {
-            StopCoroutine(rotateCoroutine);
-            rotateCoroutine = null;
-            imageToRotate.transform.localRotation = originalRotation;
-        }
+            StartCoroutine(CrackEgg());
+        }       
     }
 
-    private IEnumerator RotateCoroutine()
+    private IEnumerator CrackEgg()
     {
-        while (true)
+        transform.GetComponent<Button>().interactable = false;
+
+        float startTime = Time.time;
+        while (Time.time - startTime < 2f)
         {
             Quaternion targetRotation = Quaternion.Euler(0f, 0f, rotationAmount);
             float elapsedTime = 0f;
 
             while (Quaternion.Angle(imageToRotate.transform.localRotation, targetRotation) > 0.1f)
             {
-                imageToRotate.transform.localRotation = Quaternion.RotateTowards(imageToRotate.transform.localRotation, 
+                imageToRotate.transform.localRotation = Quaternion.RotateTowards(imageToRotate.transform.localRotation,
                     targetRotation, rotationSpeed * Time.deltaTime);
 
                 elapsedTime += Time.deltaTime;
@@ -71,5 +63,18 @@ public class EggHoverAnim : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 
             yield return new WaitForSeconds(rotationPause);
         }
+
+        StartCoroutine(ChangeImage(0.5f));
+    }
+
+    public IEnumerator ChangeImage(float duration)
+    {
+        imageToRotate.sprite = petEggSprites[currentSprite++];
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            imageToRotate.transform.localRotation = Quaternion.Slerp(imageToRotate.transform.localRotation, originalRotation, t / duration);
+            yield return null;
+        }
+        transform.GetComponent<Button>().interactable = true;
     }
 }
