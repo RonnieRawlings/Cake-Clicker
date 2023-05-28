bc1ca8e6ffbf37b9a75c4ddc381d6bc55e60a6a4
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-05-28 17:02:43 +0100

    Implemented - InfoBox Off Screen Prevention
    This commit prevents the info boxes from being displayed offscreen or partially offscreen.
    
    Next, make sure everything is working fine in build.

--- Assets/Scripts/Cursor/FollowCursor.cs
+++ Assets/Scripts/Cursor/FollowCursor.cs
@@ -9,6 +9,13 @@ public class FollowCursor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(transform.position.x, Input.mousePosition.y);
+        float bottomBound = Camera.main.transform.position.y - Camera.main.orthographicSize;
+        float topBound = Camera.main.transform.position.y + Camera.main.orthographicSize;
+
+        float y = Mathf.Clamp(Input.mousePosition.y, bottomBound, topBound + 754);
+        transform.position = new Vector2(transform.position.x, y);
+
+        Debug.Log(bottomBound);
+        Debug.Log(topBound);
     }
 }
